1b3cbc4d2d7d482c2162120413fcd6671eb9dc44
leeheain1999 <73734436+leeheain1999@users.noreply.github.com>
2020-11-14 17:21:27 +0900

    Update article.java

--- src/java20201031/article.java
+++ src/java20201031/article.java
@@ -4,13 +4,38 @@ public class Article{
 	private int id;
 	private String title;
 	private String body;
-
+	private String regDate;
+	private int hit;
+	private String writer;
+	
 	public Article(int id, String title, String body) {
 		this.id = id;
 		this.title = title;
-		this.body = body;
+			}
+
+	public String getWriter() {
+		return writer;
 	}
 
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public int getHit() {
+		return hit;
+	}
+
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	
 	public int getId() {
 		return id;
 	}
