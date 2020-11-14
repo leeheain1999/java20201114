ce3d563d40022b5b158fd89699c0e239e34986d8
leeheain1999 <73734436+leeheain1999@users.noreply.github.com>
2020-11-14 17:45:58 +0900

    Update Main.java

--- src/java20201031/Main.java
+++ src/java20201031/Main.java
@@ -1,6 +1,8 @@
 package java20201031;
 
 import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
@@ -8,10 +10,11 @@ public class Main {
 	static ArrayList<Article> articles = new ArrayList<>();
 	
 	public static int getArticleIndexById(int aid) {
+		
 		ArrayList<Article> articles = new ArrayList<>();
 
 		int existFlag = 1; // 1 없다, 2 있다.
-		int index = 0;
+		int index=0;
 
 		for (int i = 0; i < articles.size(); i++) {
 			Article article = articles.get(i);
@@ -33,10 +36,14 @@ public class Main {
 		
 		//int id = 1;
 		int id = 4;
+		
+		SimpleDateFormat format1 = new SimpleDateFormat( "yyyy.MM.dd");
+		Date time = new Date();		
+		String time1 = format1.format(time);
 
-		Article a1 = new Article(1, "제목1", "내용1");
-		Article a2 = new Article(2, "제목2", "내용2");
-		Article a3 = new Article(3, "제목3", "내용3");
+		Article a1 = new Article(1, "제목1", "내용1", time1);
+		Article a2 = new Article(2, "제목2", "내용2", time1);
+		Article a3 = new Article(3, "제목3", "내용3", time1);
 
 		articles.add(a1);
 		articles.add(a2);
@@ -58,7 +65,7 @@ public class Main {
 				String body = sc.nextLine();
 				System.out.println("게시물이 등록되었습니다.");
 				
-				Article article1 = new Article(id, title, body);
+				Article article1 = new Article(id, title, body, time1);
 				articles.add(article1);
 
 				id++;
@@ -88,7 +95,7 @@ public class Main {
 					System.out.print("내용 : ");
 					String body = sc.nextLine();
 
-					Article article2 = new Article(targetId, title, body);
+					Article article2 = new Article(targetId, title, body, time1);
 
 					articles.set(index, article2);
 				}
@@ -111,29 +118,23 @@ public class Main {
 			}
 			
 			if (cmd.equals("read")) {
-				System.out.print("원하는 게시물 번호 : ");
+				System.out.print("상세보기 게시물 번호 : ");
 				String aid = sc.nextLine();
 				int targetId = Integer.parseInt(aid);
+				int index = getArticleIndexById(targetId);
 				
-				int existFlag = 1;
-				for(int i = 0; i<articles.size(); i++) {
-					Article article = articles.get(i);
-					if(targetId == article.getId()) {
-						existFlag = 2;
-						System.out.println("====="+ targetId+"번 게시물=====");
-						System.out.println("번호 : " + article.getId());
-						System.out.println("제목 : " + article.getTitle());
-						System.out.println("내용 : " + article.getBody());
-						System.out.println("===================");
-						
-						break;
-					}
+				if(index == -1) {
+					System.out.println("없는 게시물입니다.");
+				} 
+				else {
+
+					Article article = articles.get(index);
+					System.out.println("번호 : " + article.getId());
+					System.out.println("제목 : " + article.getTitle());
+					System.out.println("내용 : " + article.getBody());
+					System.out.println("등록날짜 : " + article.getRegDate());
+					System.out.println("======================");
 				}
-				
-				if(existFlag == 1) {
-					System.out.println("게시물이 존재하지 않습니다.");
-				}
-			
 			}
 		}
 	}
