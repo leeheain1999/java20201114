6fcf55033aef8a3407b3d31b6a3f5753bb2d2e99
leeheain1999 <73734436+leeheain1999@users.noreply.github.com>
2020-11-14 17:21:58 +0900

    Update Main.java

--- src/java20201031/Main.java
+++ src/java20201031/Main.java
@@ -4,16 +4,44 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+	
 	static ArrayList<Article> articles = new ArrayList<>();
+	
+	public static int getArticleIndexById(int aid) {
+		ArrayList<Article> articles = new ArrayList<>();
 
+		int existFlag = 1; // 1 없다, 2 있다.
+		int index = 0;
+
+		for (int i = 0; i < articles.size(); i++) {
+			Article article = articles.get(i);
+			if (aid == article.getId()) {
+				existFlag = 2;
+				index = i;
+			}
+			else {
+				index = -1; // -1 없다.
+			}
+		}
+
+		return index;
+
+	}
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		ArrayList<Article> articles = new ArrayList<>();
+		
+		//int id = 1;
+		int id = 4;
 
-		int index = 0;
-		int id = 1;
+		Article a1 = new Article(1, "제목1", "내용1");
+		Article a2 = new Article(2, "제목2", "내용2");
+		Article a3 = new Article(3, "제목3", "내용3");
 
+		articles.add(a1);
+		articles.add(a2);
+		articles.add(a3);
+		
 		while (true) {
 			System.out.print("명령어를 입력해주세요 : ");
 			String cmd = sc.nextLine();
@@ -50,47 +78,36 @@ public class Main {
 				System.out.print("수정할 게시물 번호 : ");
 				String aid = sc.nextLine();
 				int targetId = Integer.parseInt(aid);
-				
-				int existFlag = 1;
-				for(int i = 0; i<articles.size(); i++) {
-					Article article = articles.get(i);
-					if(targetId == article.getId()) {
-						existFlag = 2;
-						System.out.print("제목: ");
-						String title = sc.nextLine();
-						System.out.print("내용: ");
-						String body = sc.nextLine();
-						Article article2 = new Article(targetId, title, body);
-						articles.set(i, article2);
-						
-						break;
-					}
-				}
-				
-				if(existFlag == 1) {
+				int index = getArticleIndexById(targetId);
+
+				if (index == -1) {
 					System.out.println("없는 게시물입니다.");
+				} else {
+					System.out.print("제목 : ");
+					String title = sc.nextLine();
+					System.out.print("내용 : ");
+					String body = sc.nextLine();
+
+					Article article2 = new Article(targetId, title, body);
+
+					articles.set(index, article2);
 				}
+
+
 			}
-			
+				
 			if (cmd.equals("delete")) {
 				System.out.print("삭제할 게시물 번호 : ");
 				String aid = sc.nextLine();
 				int targetId = Integer.parseInt(aid);
-				
-				int existFlag = 1;
-				for(int i = 0; i<articles.size(); i++) {
-					Article article = articles.get(i);
-					if(targetId == article.getId()) {
-						existFlag = 2;
-						articles.remove(i);
-						System.out.println("삭제가 완료되었습니다.");
-						break;
-					}
-				}
-				
-				if(existFlag == 1) {
+				int index = getArticleIndexById(targetId);
+				if (index == -1) {
 					System.out.println("없는 게시물입니다.");
+				} else {
+					articles.remove(index);
+					System.out.println("삭제가 완료되었습니다.");
 				}
+
 			}
 			
 			if (cmd.equals("read")) {
